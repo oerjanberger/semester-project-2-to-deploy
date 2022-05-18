4810846199cbf34a9911d5247d9469ac528601eb
oerjanberger <oerjan4@gmail.com>
2022-05-18 14:18:40 +0200

    updated with all product btn

--- js/favorites.js
+++ js/favorites.js
@@ -8,13 +8,15 @@ renderFavorites()
 const allProductsBtn = document.querySelector(".all__products__link__container");
 // Added because there where issues with the button where not directing the mobile users
 allProductsBtn.addEventListener("click", () => {
-    location.href = "/products.html"
+    location.href = "products.html"
 });
 
-allProductsBtn.addEventListener("mouseenter", e => {
-    allProductsBtn.innerHTML = `<p>View all products</p> <img src="./logo/flying_stork.svg" alt="Babybliss stork logo"class="logo__btn flying__logo">`;
-});
-allProductsBtn.addEventListener("mouseleave", e => {
-    allProductsBtn.innerHTML = `<p>View all products</p> <img src="./logo/Stork_SVG.svg" alt="Babybliss stork logo"class="logo__btn">`;
-});
+if (screen.width > 1024) {
+    allProductsBtn.addEventListener("mouseenter", () => {
+        allProductsBtn.innerHTML = `<p>View all products</p> <img src="./logo/flying_stork.svg" alt="Babybliss stork logo"class="logo__btn flying__logo">`;
+    });
+    allProductsBtn.addEventListener("mouseleave", () => {
+        allProductsBtn.innerHTML = `<p>View all products</p> <img src="./logo/Stork_SVG.svg" alt="Babybliss stork logo"class="logo__btn">`;
+    });
+}
 
