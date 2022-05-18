3d56bef9b1f7022a9070ec63b75e8e443c9fb9f6
oerjanberger <oerjan4@gmail.com>
2022-05-18 13:47:27 +0200

    updated all products btn

--- js/favorites.js
+++ js/favorites.js
@@ -6,6 +6,10 @@ createNav();
 renderFavorites()
 
 const allProductsBtn = document.querySelector(".all__products__link__container");
+// Added because there where issues with the button where not directing the mobile users
+allProductsBtn.addEventListener("click", () => {
+    location.href = "/products.html"
+});
 
 allProductsBtn.addEventListener("mouseenter", e => {
     allProductsBtn.innerHTML = `<p>View all products</p> <img src="./logo/flying_stork.svg" alt="Babybliss stork logo"class="logo__btn flying__logo">`;
@@ -14,7 +18,3 @@ allProductsBtn.addEventListener("mouseleave", e => {
     allProductsBtn.innerHTML = `<p>View all products</p> <img src="./logo/Stork_SVG.svg" alt="Babybliss stork logo"class="logo__btn">`;
 });
 
-// Added because there where issues with the button where not directing the mobile users
-allProductsBtn.addEventListener("click", () => {
-    location.href = "/products.html"
-});
--- js/index.js
+++ js/index.js
@@ -47,6 +47,10 @@ setTimeout(function () {
 
 const allProductsBtn = document.querySelector(".all__products__link__container");
 
+// Added because there where issues with the button where not directing the mobile users
+allProductsBtn.addEventListener("click", () => {
+    location.href = "/products.html"
+});
 
 allProductsBtn.addEventListener("mouseenter", e => {
     allProductsBtn.innerHTML = `<p>View all products</p> <img src="./logo/flying_stork.svg" alt="Babybliss stork logo"class="logo__btn flying__logo">`;
@@ -55,8 +59,5 @@ allProductsBtn.addEventListener("mouseleave", e => {
     allProductsBtn.innerHTML = `<p>View all products</p> <img src="./logo/Stork_SVG.svg" alt="Babybliss stork logo"class="logo__btn">`;
 });
 
-// Added because there where issues with the button where not directing the mobile users
-allProductsBtn.addEventListener("click", () => {
-    location.href = "/products.html"
-});
+
 
