61feb95c3e575ae60da72818fdf1fabd9146718e
oerjanberger <oerjan4@gmail.com>
2022-05-20 12:59:42 +0200

    updated

--- js/components/search/filterProducts.js
+++ js/components/search/filterProducts.js
@@ -6,14 +6,13 @@ export default function filterProducts() {
 
     filterInput.oninput = function () {
         filterValue = filterInput.value.toLowerCase().trim();
-        filterBtnContainer.innerHTML = `<a href="search.html?search=${filterValue}"><button class="filter__btn"><i
+        filterBtnContainer.innerHTML = `<a href="search.html?search=${filterValue}"><button type="button" class="filter__btn"><i
         class="fas fa-search"></i></button></a>`;
     }
 
-    filterBtn.onclick = function (event) {
-        event.preventDefault()
+    filterBtn.onclick = function () {
         filterValue = filterInput.value.toLowerCase().trim();
-        filterBtnContainer.innerHTML = `<a href="search.html?search=${filterValue}"><button class="filter__btn"><i
+        filterBtnContainer.innerHTML = `<a href="search.html?search=${filterValue}"><button type="button" class="filter__btn"><i
         class="fas fa-search"></i></button></a>`;
     }
 }
